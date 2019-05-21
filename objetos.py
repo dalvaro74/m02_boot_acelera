e4b4581637325cb03670cbbc211cfc73abc6304e
dalvaro74 <dalvaro74@gmail.com>
2019-05-22 01:01:35 +0200

    objetos tortuga

---
+++ objetos.py
@@ -0,0 +1,11 @@
+import turtle
+
+miTortuga = turtle.Turtle()
+
+miTortuga.fd(50)
+miTortuga.shape('turtle')
+
+tuTortuga = turtle.Turtle()
+tuTortuga.color('green')
+tuTortuga.left(90)
+tuTortuga.fd(50)
